9cad1712d623bfb9162b460c650cea79141afe0b
lucascorreaaa <lucascorreasilvaa@gmail.com>
2017-09-22 21:38:13 -0300

    Linkando as 3 partes principais do App (apresentacao, cadastro e workspace)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+
+  get 'users/new'
+
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
 end
