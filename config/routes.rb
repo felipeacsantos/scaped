65373d406ee23b4e0690c817c4f010c093a026ce
Victor <seven.ray@hotmail.com>
2017-09-24 10:27:39 +0000

    Autenticação com Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/index'
-
-  get 'users/new'
-
+  devise_for :users
+  
   get 'home/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'home#index'
 end
