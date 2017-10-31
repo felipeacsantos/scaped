bbfe2c3ea37ea3fb72e02dd2f037e4bf0b87eeee
lucascorreaaa <lucascorreasilvaa@gmail.com>
2017-10-31 11:17:24 -0200

    Integração de template da home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   resources :objetos
   resources :usuarios
   get 'sessions/index'
-
   get 'users/new'
-
   get 'home/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'layouts/workspace'
 
   root 'home#index'
 end
