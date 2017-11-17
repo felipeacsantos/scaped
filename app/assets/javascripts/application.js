a822b5ed67cb678eb469a25ecb0e7004785cbbfc
Felipe Augusto <felipe.dossantos@fluig.com>
2017-11-17 21:03:31 -0200

    a couple other things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,22 +74,22 @@ function getParamByObjectType(event){
     url: "http://localhost:3000/parametros/to/"+event.target.selectedIndex+".json",
     success: function(data){
       for(i = 0; i < data.length; i++){
-        fields_string += addSpecificFields(data[i]);
-
+        fields_string = addSpecificFields(data[i]);
+        add_fields(event.target, "objetovalparametros", fields_string)
         //$(event.target).hide();
       }
-      add_fields(event.target, "objetovalparametros", fields_string)
+
 
     }
   });
 }
 
 function addSpecificFields(data){
-    var fields = "<div class='control-group nested-fields'><div class='controls'><select class='parametro' onchange='myFunction(event)' name='objeto[objetovalparametros_attributes][new_objetovalparametros][valparametros][parametro_id]' id='objeto_objetovalparametros_attributes_new_objetovalparametros_valparametros_parametro_id' disabled='disabled'>";
+      var fields = "<div class='control-group nested-fields'><div class='controls'><select class='parametro' onchange='myFunction(event)' name='objeto[objetovalparametros_attributes][new_objetovalparametros][valparametros][parametro_id]' id='objeto_objetovalparametros_attributes_new_objetovalparametros_valparametros_parametro_id'>";
           fields +=  "<option value='"+data.id+"' selected='selected'>"+data.name+"<\/option>";
         fields += "<\/select>"
 
-        fields += "<select class='parametro' onchange='myFunction(event)' name='objeto[objetovalparametros_attributes][new_objetovalparametros][valparametros][id]' id='objeto_objetovalparametros_attributes_new_objetovalparametros_valparametros_id'>";
+        fields += "<select class='parametro' onchange='myFunction(event)' name='objeto[objetovalparametros_attributes][new_objetovalparametros][valparametro_id]' id='objeto_objetovalparametros_attributes_new_objetovalparametros_valparametros_valparametro_id'>";
         for(i2 = 0; i2 < data.valparametros.length; i2++){
           fields += "<option value='"+data.valparametros[i2].id+"'>"+data.valparametros[i2].valor+"<\/option>";
         }
